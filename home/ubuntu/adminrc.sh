7e550e9770f7752cf8d861d3f6c4d28b2cadbd42
secobau <sebastian.colomar@gmail.com>
2020-08-03 07:17:02 +0200

    Create adminrc.sh

---
+++ home/ubuntu/adminrc.sh
@@ -0,0 +1,9 @@
+export OS_AUTH_TYPE=password
+export OS_USERNAME=admin
+export OS_PASSWORD=admin
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_AUTH_URL=http://hostname:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
